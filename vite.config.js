41bc00dc22f39fe08ea2d4ff091008b1a9f85ec2
MADLANCE45 <marcocretu0@gmail.com>
2026-08-30 18:17:44 +0200

    TEST

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    nodePolyfills({
+      // Inietta i moduli che servono a Solana e Web3
+      include: ['crypto', 'stream', 'buffer'],
+    }),
   ],
+  define: {
+    // Risolve un altro errore classico di Solana ("global is not defined")
+    global: 'window',
+  }
 })
